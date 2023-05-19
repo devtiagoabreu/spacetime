2be783d1b63ddf1886daf495ee3b432b4afa5c7b
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-05-19 11:43:27 -0300

    criando projeto e configurando o backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
+/* eslint-disable prettier/prettier */
 import fastfy from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastfy()
+const prisma = new PrismaClient()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('🧑‍🚀 HTTP server running on http//localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🧑‍🚀 HTTP server running on http//localhost:3333')
+  })
