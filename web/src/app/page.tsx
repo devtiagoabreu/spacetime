6c762c81e304074d71aa410fe718b793c6a1cf7d
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-05-22 14:56:44 -0300

    Lista de memórias vazia

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* LEFT */}
+      <div className="flexflex-col items-start justify-between px-28 py-16"></div>
+
+      {/* HIGHT */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não publicou nenhum post no blog, {'    '}
+            <a href="" className="undeline hover:text-gray-50">
+              publicar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
