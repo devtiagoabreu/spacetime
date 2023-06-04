d87dd799df23d308a45bed5808d3baebc2e16ab0
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-06-04 11:51:50 -0300

    troca do texto do botão

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
             className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
             href=""
           >
-            INICIAR UMA POSTAGEM
+            INICIAR UM DISCURSO
           </a>
         </div>
         {/* COPYRIGHT */}
