d39c084961cde15a9daa1f3a644bec6ba1ad19f3
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-06-17 18:56:52 -0300

    obtendo github code web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
         {/* SIGN IN */}
         <a
-          href=""
+          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
           className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
         >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
