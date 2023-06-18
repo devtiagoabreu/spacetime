5c2d378bffe9dc570bb2622b2d9094c18f633ee1
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-06-18 11:46:47 -0300

    recebendo access_token do github

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
 /* eslint-disable prettier/prettier */
+import 'dotenv/config'
 import fastfy from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { usersRoutes } from './routes/users'
+import { authRoutes } from './routes/auth'
 
 const app = fastfy()
 
 app.register(cors, {
   origin: true,
 })
-app.register(memoriesRoutes, usersRoutes)
+
+app.register(jwt, {
+  secret: 'd87hb-kx7x9-dqgh3-ffrdg-ym2d6',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+app.register(usersRoutes)
 
 app
   .listen({
